321ecd7a14461fcbc45ed663193536f28e35339b
bahubaliyh <bahubali@ewar.in>
2025-06-20 18:08:27 +0530

    fixed vite config issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/browser-extension-manager-reactJs/', // 👈 set this to your repo name with slashes
 })
